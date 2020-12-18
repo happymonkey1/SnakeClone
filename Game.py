c3ce7c3610b0780f013777e2f61e3e3c6d393352
happymonkey1 <brownejamessean@gmail.com>
2020-12-17 17:01:47 -0800

    finished game

--- Game.py
+++ Game.py
@@ -1,5 +1,7 @@
 import pygame
 from Player import Player
+from random import random
+from math import floor
 #https://realpython.com/pygame-a-primer/#pygame-concepts
 
 #import keybind locals from pygame
@@ -19,18 +21,23 @@ pygame.init()
 
 #store the width and height of our window
 SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
-
-#set our target frame rate
-TARGET_FRAMERATE = 60
+SCREEN_HEIGHT = 800
 
 #color of the background of our window
 BACKGROUND_COLOR = (113,111,133)
 
+#init variables for grid
+ROWS, COLUMNS = 20, 20
+
+GRID_WIDTH = SCREEN_WIDTH/ROWS
+GRID_HEIGHT = SCREEN_HEIGHT/COLUMNS
+
 #initialize the pygame screen and store it
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Snake Game")
-player = Player()
+
+#init player
+player = Player(GRID_WIDTH, GRID_HEIGHT)
 player.setWindowDimensions(SCREEN_WIDTH, SCREEN_HEIGHT)
 
 #boolean for whether our game is running
@@ -43,6 +50,20 @@ delta_time = 0
 
 #store pygame clock 
 clock = pygame.time.Clock()
+
+#set our target frame rate
+TARGET_FRAMERATE = 144
+
+#apple
+def new_random_apple_spawn():
+    return (floor(random() * SCREEN_WIDTH / GRID_WIDTH), floor(random() * SCREEN_HEIGHT / GRID_HEIGHT))
+
+def draw_apple(surf, pos):
+    screen.blit(surf, (pos[0] * GRID_WIDTH, pos[1] * GRID_HEIGHT))
+    
+apple_pos = new_random_apple_spawn()
+apple_surf = pygame.Surface((GRID_WIDTH, GRID_HEIGHT))
+apple_surf.fill((255,0,0))
 
 #game loop
 while game_running:
@@ -57,7 +78,11 @@ while game_running:
 
     #fill the screen with our background color
     screen.fill(BACKGROUND_COLOR)
+    draw_apple(apple_surf, apple_pos)
+    
     player.update(delta_time)
+    if player.check_apple(apple_pos):
+        apple_pos = new_random_apple_spawn()
     player.draw(screen)
     
     # Look at every event in the queue
--- Player.py
+++ Player.py
@@ -3,55 +3,119 @@ import pygame
 from pygame.locals import (K_UP, K_DOWN, K_LEFT, K_RIGHT, K_ESCAPE, KEYDOWN, QUIT, K_w, K_a, K_s, K_d)
 class Player(pygame.sprite.Sprite):
 
-    def __init__(self):
+    def __init__(self, grid_width, grid_height):
         super(Player, self).__init__()
-        self.playerWidth = 25
-        self.playerHeight = 25
+        self.playerWidth = grid_width
+        self.playerHeight = grid_height
         self.surf = pygame.Surface((self.playerWidth, self.playerHeight))
         self.surf.fill((50, 205, 50))
         self.rect = self.surf.get_rect()
-        self.speed = 100
+        self.speed = 15 #grid changes per second
+        self._internal_clock = 0
         self.direction = (0,-1)
+        self.last_direction = (0,-1)
         self.isDead = False
+        
+        self.GRID_WIDTH = grid_width
+        self.GRID_HEIGHT = grid_height
+        
+        self.tail = []
+        
 
     def setWindowDimensions(self, width, height):
         self.screenWidth = width
         self.screenHeight = height
         self.xPosition = width / 2
         self.yPosition = height / 2
+        
+        self.calcAndSetGridPos(self.xPosition, self.yPosition)
 
     def draw(self, screen):
-        screen.blit(self.surf, (self.xPosition, self.yPosition))
+        for piece in self.tail:
+            x,y = piece
+            screen.blit(self.surf, (x * self.GRID_WIDTH, y * self.GRID_HEIGHT))
 
     #determines what direction the snake goes based on the buttons you press
     def controls(self):
         pressed_keys = pygame.key.get_pressed()
         if pressed_keys[K_UP] or pressed_keys[K_w]:
-            self.changeDirection(0,-1)
+            if self.get_last_direction()[1] != 1:
+                self.changeDirection(0,-1)
         elif pressed_keys[K_DOWN] or pressed_keys[K_s]:
-            self.changeDirection(0,1)
+            if self.get_last_direction()[1] != -1:
+                self.changeDirection(0,1)
         elif pressed_keys[K_LEFT] or pressed_keys[K_a]:
-            self.changeDirection(-1,0)
+            if self.get_last_direction()[0] != 1:
+                self.changeDirection(-1,0)
         elif pressed_keys[K_RIGHT] or pressed_keys[K_d]:
-            self.changeDirection(1,0)
+            if self.get_last_direction()[0] != -1:
+                self.changeDirection(1,0)
             
     def changeDirection(self, x, y):
         self.direction = (x,y)
 
     def update(self, delta_time):
         self.controls()
-        self.xPosition += self.speed * self.getXDirection() * delta_time
-        self.yPosition += self.speed * self.getYDirection() * delta_time
+        # self.xPosition += self.speed * self.getXDirection() * delta_time
+        # self.yPosition += self.speed * self.getYDirection() * delta_time
+        
+        
+        nextMove = 1 / self.speed
+        if self._internal_clock >= nextMove:
+            self.move()
+            self._internal_clock = 0
+        else:
+            self._internal_clock += delta_time
+
+    def move(self):
+        
+        
+        tail = self.tail.pop(len(self.tail)-1)
+        head_x, head_y = self.tail[0] if len(self.tail) > 0 else tail
+        
+        move_grid_x, move_grid_y = head_x + self.getXDirection(), head_y + self.getYDirection()
+        self.last_direction = (self.getXDirection(), self.getYDirection())
+        self.xPosition, self.yPosition = move_grid_x * self.GRID_WIDTH, move_grid_y * self.GRID_HEIGHT
+        self.tail.insert(0,(move_grid_x, move_grid_y))
+        
+        self.check_tail_collision()
         self.ifOnEdgeDie()
+        
+    def update_tail(self):
+        for index in range(len(self.tail)-1, -1, -1):
+            self.tail[index] = self.tail[index-1]
 
     def getXDirection(self):
         return self.direction[0]
 
     def getYDirection(self):
         return self.direction[1]
+    
+    def get_last_direction(self):
+        return self.last_direction
+    
+    def calcAndSetGridPos(self, x, y):
+        self.tail.append((x / self.GRID_WIDTH, y / self.GRID_HEIGHT))
 
     def ifOnEdgeDie(self):
         if self.xPosition + self.playerWidth > self.screenWidth or self.xPosition < 0:
             self.isDead = True
         if self.yPosition + self.playerHeight > self.screenHeight or self.yPosition < 0:
             self.isDead = True
+            
+    def check_apple(self, pos):
+        x,y = self.tail[0]
+        if pos[0] == x and pos[1] == y:
+            self.tail.insert(0, (x, y))
+            return True
+        else:
+            return False
+        
+    def check_tail_collision(self):
+        head_x, head_y = self.tail[0]
+        for index, tail_part in enumerate(self.tail):
+            if index != 0:
+                tail_x, tail_y = tail_part
+                if head_x == tail_x and head_y == tail_y:
+                    self.isDead = True
+                
