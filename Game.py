3092af7a04917cb5b40cf2b849419853c930eae3
M-atthew-L <matthewlee080807@gmail.com>
2020-12-19 21:28:33 -0800

    Snake Game Updates: Grid and apple added

--- Game.py
+++ Game.py
@@ -1,4 +1,6 @@
 import pygame
+import random
+import math
 from Player import Player
 #https://realpython.com/pygame-a-primer/#pygame-concepts
 
@@ -19,10 +21,24 @@ pygame.init()
 
 #store the width and height of our window
 SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
+SCREEN_HEIGHT = 800
 
+rows = 20
+columns = 20
+
+gridWidth = SCREEN_WIDTH / rows
+gridHeight = SCREEN_HEIGHT / columns
+
+def randomAppleSpawn():
+    x = math.floor(random.random() * SCREEN_WIDTH / gridWidth)
+    y = math.floor(random.random() * SCREEN_HEIGHT / gridHeight)
+    return (x,y)
+
+applePosition = randomAppleSpawn()
+appleSurface = pygame.Surface((gridHeight, gridHeight))
+appleSurface.fill((255,0,0))
 #set our target frame rate
-TARGET_FRAMERATE = 60
+TARGET_FRAMERATE = 30
 
 #color of the background of our window
 BACKGROUND_COLOR = (113,111,133)
@@ -30,31 +46,41 @@ BACKGROUND_COLOR = (113,111,133)
 #initialize the pygame screen and store it
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Snake Game")
-player = Player()
+player = Player(gridWidth, gridHeight)
 player.setWindowDimensions(SCREEN_WIDTH, SCREEN_HEIGHT)
 
 #boolean for whether our game is running
 game_running = True
+lastFrameTime = 0
+currentFrameTime = 0
+deltaTime = 0
 
 #game loop
 while game_running:
     #clamp the maximum number of frames our game can have per second
     pygame.time.Clock().tick(TARGET_FRAMERATE)
 
+    currentFrameTime = pygame.time.get_ticks()
+    deltaTime = (currentFrameTime - lastFrameTime) / 1000
+    
     #fill the screen with our background color
     screen.fill(BACKGROUND_COLOR)
-    player.update()
+    player.update(deltaTime)
     player.draw(screen)
+    screen.blit(appleSurface, (applePosition[0] * gridWidth, applePosition[1] * gridHeight))
     
     # Look at every event in the queue
     for event in pygame.event.get():
         #stop the game loop if the quit event is received
         if event.type == QUIT:
             game_running = False
+    
 
     
     #allow any graphical changes to display to screen
     pygame.display.flip()
+
+    lastFrameTime = currentFrameTime
 '''
     if player.isDead:
         game_running = False
--- Player.py
+++ Player.py
@@ -1,28 +1,34 @@
 #CODE FOR PLAYER
 import pygame
 import random
+import math
 from pygame.locals import (K_UP, K_DOWN, K_LEFT, K_RIGHT, K_ESCAPE, KEYDOWN, QUIT, K_w, K_a, K_s, K_d)
 class Player(pygame.sprite.Sprite):
 
-    def __init__(self):
+    def __init__(self, gridWidth, gridHeight):
         super(Player, self).__init__()
-        self.playerWidth = 25
-        self.playerHeight = 25
+        self.playerWidth = gridWidth
+        self.playerHeight = gridHeight
+        self.gridWidth = gridWidth
+        self.gridHeight = gridHeight
         self.surf = pygame.Surface((self.playerWidth, self.playerHeight))
         self.surf.fill((50, 205, 50))
         self.rect = self.surf.get_rect()
         self.speed = 5
         self.direction = random.choice([(0, -1), (0, 1), (-1, 0), (1, 0)])
         self.isDead = False
+        self.internalClock = 0
 
     def setWindowDimensions(self, width, height):
         self.screenWidth = width
         self.screenHeight = height
         self.xPosition = width / 2
         self.yPosition = height / 2
+        self.xGridPosition = math.floor(self.xPosition / self.gridWidth)
+        self.yGridPosition = math.floor(self.yPosition / self.gridHeight)
 
     def draw(self, screen):
-        screen.blit(self.surf, (self.xPosition, self.yPosition))
+        screen.blit(self.surf, (self.xGridPosition * self.gridWidth, self.yGridPosition * self.gridHeight))
 
     #determines what direction the snake goes based on the buttons you press
     def controls(self):
@@ -39,11 +45,19 @@ class Player(pygame.sprite.Sprite):
     def changeDirection(self, x, y):
         self.direction = (x,y)
 
-    def update(self):
+    def update(self, deltaTime):
         self.controls()
-        self.xPosition += self.speed * self.getXDirection()
-        self.yPosition += self.speed * self.getYDirection()
-        self.ifOnEdgeDie()
+        nextMove = 1 / self.speed
+        if self.internalClock >= nextMove:
+            #self.xPosition += self.speed * self.getXDirection()
+            #self.yPosition += self.speed * self.getYDirection()
+            self.xGridPosition += self.getXDirection()
+            self.yGridPosition += self.getYDirection()
+            self.ifOnEdgeDie()
+            self.internalClock -= nextMove
+        else:
+            self.internalClock += deltaTime
+
 
     def getXDirection(self):
         return self.direction[0]
@@ -52,16 +66,21 @@ class Player(pygame.sprite.Sprite):
         return self.direction[1]
 
     def ifOnEdgeDie(self):
-        if self.xPosition + self.playerWidth > self.screenWidth or self.xPosition < 0:
+        if self.xGridPosition * self.gridWidth > self.screenWidth or self.xGridPosition < 0:
             self.isDead = True
             self.reset()
-        if self.yPosition + self.playerHeight > self.screenHeight or self.yPosition < 0:
+        if self.yGridPosition * self.gridHeight > self.screenHeight or self.yGridPosition < 0:
             self.isDead = True
             self.reset()
     
     def reset(self):
         if self.isDead:
-            self.xPosition = self.screenWidth / 2
-            self.yPosition = self.screenHeight / 2
+            self.xGridPosition = math.floor(self.screenWidth / 2 / self.gridWidth)
+            self.yGridPosition = math.floor(self.screenHeight / 2 / self.gridHeight)
             self.direction = random.choice([(0, -1), (0, 1), (-1, 0), (1, 0)])
+            self.internalClock = 0
             self.isDead = False
+
+    #Add collision/snake eating apple, then set apple to new random positions not on the snake
+    #Spawn extra snake part at the back of the the snake
+    #Make sure all parts of the snake moves not just the head
